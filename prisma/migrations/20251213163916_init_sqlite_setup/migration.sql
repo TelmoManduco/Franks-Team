496364502e5adfd21a59f5b5fb3f2e15eb2d03bc
Telmo <telmosergiomanduco2015@gmail.com>
2025-12-13 17:17:56 +0000

    update

--- prisma/migrations/20251213163916_init_sqlite_setup/migration.sql
+++ prisma/migrations/20251213163916_init_sqlite_setup/migration.sql
@@ -1,12 +1,10 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT,
     "email" TEXT NOT NULL,
-    "passwordHash" TEXT NOT NULL,
-    "role" TEXT NOT NULL DEFAULT 'user',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateIndex
