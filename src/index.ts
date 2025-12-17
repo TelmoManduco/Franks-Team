96a91371f9e8916c5c12b73523c11ee327b7c1e4
Telmo <telmosergiomanduco2015@gmail.com>
2025-12-17 00:27:00 +0000

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import authRoutes from "./routes/auth";
-import { json } from "node:stream/consumers";
+import testRoutes from "./routes/test";
 
 const app = express();
+const PORT = 3000;
 
+// ✅ ESTE MIDDLEWARE É O QUE FALTA
 app.use(express.json());
 
-// API routes
+// routes
 app.use("/api", authRoutes);
+app.use("/api", testRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
